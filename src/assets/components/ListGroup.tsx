d75c66743b6f0283d9e4164674855b2f8a39c423
meron847 <belachewmeron15@gmail.com>
2025-08-15 16:50:36 +0100

    giving onClick element

--- src/assets/components/ListGroup.tsx
+++ src/assets/components/ListGroup.tsx
@@ -1,14 +1,20 @@
-
-
 function ListGroup() {
-      const items =  [];
- 
+  let items = ["New York", "SanFrancisco", "Tokyo"];
+
   return (
     <>
       <h1>List</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-       {items.map(item => <li key={item}>{item}</li>)}
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log(item, index)}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
